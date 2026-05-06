4de2b2da2724aba5af587c2573119f5445599ca6
BogdanATA <batameev@gmail.com>
2026-05-06 11:44:54 -0400

    String format created

---
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class NameFormatter {
+    private String prefix;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+
+    private NameFormatter() {}
+
+
+
+    public static String format(String prefix, String firstName, String middleName,
+                                String lastName, String suffix) {
+        String formattedName = lastName + ", ";
+
+        if (!prefix.isBlank()) {
+            formattedName += prefix + " ";
+        }
+
+        formattedName += firstName;
+
+        if (!middleName.isBlank()) {
+            formattedName += middleName + ", ";
+        }
+
+        if (!suffix.isBlank()) {
+            formattedName += suffix;
+        }
+
+        return formattedName;
+    }
+}
